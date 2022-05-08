706c1589227baf56d6eac32ceeefc735174b78cd
MarekStolz <stolzm95@gmail.com>
2022-05-08 22:10:13 +0200

    init

--- TEXT_SIF/index.php
+++ TEXT_SIF/index.php
@@ -1,64 +1,51 @@
-<?php
-$cryptArray = array( "a"=>"x",
-"b"=>"h",
-"c"=>"y",
-"d"=>"z",
-"f"=>"j",
-"e"=>"v",
-"g"=>"a",
-"h"=>"b",
-"i"=>"c",
-"j"=>"d",
-"k"=>"f",
-"n"=>"r",
-"m"=>"e",
-"l"=>"i",
-"o"=>"k",
-"p"=>"n",
-"s"=>"w",
-"r"=>"s",
-"t"=>"m",
-"u"=>"l",
-"v"=>"o",
-"w"=>"p",
-"x"=>"q",
-"y"=>"t",
-""=>"1",
-"z"=>"u");
-
-$string = "marekjefrajer";
-echo"$string";
-
-function cryptit($string,$cryptArray){    
-   
-    $temp = "";     
-
-    $length = strlen($string);       
-   
-    for($i=0; $i<$length; $i++){       
-      
-        $temp .= $cryptArray[$string[$i]];
-    }
-   
-    return $temp;
-
-} 
-
-function decryptit($string,$cryptArray){
-    $temp = "";
-    $length = strlen($string);    
-   
-    $cryptArray = array_flip($cryptArray);     
-   
-    for($i =0; $i<$length; $i = $i+3){       
-        $temp .= $cryptArray[$string[$i+3]];
-    }
-    return $temp;
-}
-
-$crypted =  cryptit($string,$cryptArray);
-echo " = $crypted";
-
-$decrypted = decryptit($crypted,$cryptArray);
-echo $decrypted;     
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php include 'project.php'; ?>
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <div class="container">
+                
+                <div class="row">
+                    <div class="col-lg-6 text-center">
+                        <select name="cesar" class="custom-select-sm">
+                            <option value="cesar">Caesar cipher</option>
+                        </select>
+                    </div>                
+                    <div class="col-lg-6">
+                        <label for="key">KEY: </label>
+                        <input type="text" class="key" name="key" id="key">
+                    </div>    
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-lg-8">
+                        <label for="origin">TEXT</label><br>
+                        <textarea name="origin" id="origin"><?php if(isset($_POST['cipher']) && $_POST['cipher'] === "cesar" && $_POST['decrypt']) { decesar();}?></textarea>
+                    </div>
+                    <div class="col-lg-4 text-center">
+                        <button class="btn btn-primary first" name="encrypt">Encrypt</button><br>
+                    </div>                
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-8">
+                        <label for="encrypted">SIF TEXT</label><br>
+                        <textarea name="encrypted" id="encrypted"><?php 
+                        if(isset($_POST['cipher']) && $_POST['cipher'] === "cesar") {
+                            cesar();
+                        }?></textarea>
+                    </div> 
+                 
+                    <div class="col-lg-4 text-center">
+                        <button class="btn btn-danger" name="decrypt" value="1">Decrypt</button><br>
+                    </div>  
+              </div>        
+           </div>
+            
+          
+    </body>
+</html>
